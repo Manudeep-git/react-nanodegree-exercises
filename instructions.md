05b4d12cd24f53a92340d6faea21ab2863fe28e6
root <manudeepgadde01@gmail.com>
2020-06-20 01:13:53 +0000

    Fourth exercise and instructions

--- instructions.md
+++ instructions.md
@@ -1,31 +1,9 @@
-# Instructions
+### Instructions
 
-Let's do something a little bit more complicated. Instead of displaying a
-list of users and their movies, this time you need to display a list of movies.
+**Task**: Edit the code to make the printed text mirror what we type into the input
+field. When we erase all of the text, nothing should be printed to the screen.
 
-For each movie in the list, there are two options:
+Remember that the React component that renders the form also controls what
+happens in that form on user input.
 
-1. If the movie has been favorited, then display a list of all of the users who said that this movie was their favorite.
-2. If the movie has *not* been favorited, display some text stating that no one favorited the movie.
-
-As you go about tackling this project, try to make the app *modular* by breaking it into resusable React components.
-
-## Example
-
-```html
-<h2>Forrest Gump</h2>
-<p>Liked By:</p>
-<ul>
-  <li>Nicholas Lain</li>
-</ul>
-
-<h2>Get Out</h2>
-<p>Liked By:</p>
-<ul>
-  <li>John Doe</li>
-  <li>Autumn Green</li>
-</ul>
-
-<h2>Autumn Green</h2>
-<p>None of the current users liked this movie</p>
-```
+This exercise will help you solidify what you've learned about Controlled Components.
--- src/App.js
+++ src/App.js
@@ -1,132 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Dashboard from "./Dashboard"
-/*
-Display a list of movies where each movie contains a list of users that favorited it.
-For detailed instructions, refer to instructions.md.
-*/
-
-const profiles = [
-  {
-    id: 1,
-    userID: '1',
-    favoriteMovieID: '1',
-  },
-  {
-    id: 2,
-    userID: '2',
-    favoriteMovieID: '1',
-  },
-  {
-    id: 3,
-    userID: '4',
-    favoriteMovieID: '5',
-  },
-  {
-    id: 4,
-    userID: '5',
-    favoriteMovieID: '2',
-  },
-  {
-    id: 5,
-    userID: '3',
-    favoriteMovieID: '5',
-  },
-  {
-    id: 6,
-    userID: '6',
-    favoriteMovieID: '4',
-  },
-];
-
-const users = {
-  1: {
-    id: 1,
-    name: 'Jane Jones',
-    userName: 'coder',
-  },
-  2: {
-    id: 2,
-    name: 'Matthew Johnson',
-    userName: 'mpage',
-  },
-  3: {
-    id: 3,
-    name: 'Autumn Green',
-    userName: 'user123',
-  },
-  4: {
-    id: 3,
-    name: 'John Doe',
-    userName: 'user123',
-  },
-  5: {
-    id: 5,
-    name: 'Lauren Carlson',
-    userName: 'user123',
-  },
-  6: {
-    id: 6,
-    name: 'Nicholas Lain',
-    userName: 'user123',
-  },
-};
-
-const movies = {
-  1: {
-    id: 1,
-    name: 'Planet Earth',
-  },
-  2: {
-    id: 2,
-    name: 'Selma',
-  },
-  3: {
-    id: 3,
-    name: 'Million Dollar Baby',
-  },
-  4: {
-    id: 4,
-    name: 'Forrest Gump',
-  },
-  5: {
-    id: 5,
-    name: 'Get Out',
-  },
-};
 
 class App extends Component {
-  constructor(props){
-  super(props);
-  this.usersPerMovie={}
-  /*Iterating over movies,getting their id and comparing with profiles userid can be an option 
-   but since profiles is an list, we will get the userid's  for each movie id*/
-    profiles.forEach(profile => 
-     {
-     const movieId=profile.favoriteMovieID;
-     if(this.usersPerMovie[movieId])
-     {
-    	    this.usersPerMovie[movieId].push(users[profile.userID].name);
-    	}
-     else
-     {
-     this.usersPerMovie[movieId]=[users[profile.userID].name];
-     }
-    });
+  
+  state = {
+    userInput : '',
   }
+
+  updateInput = (input) => {
+  	this.setState({
+    	userInput: input.trim()
+    })
+  }
+ 
+
   render() {
+    const {userInput}=this.state
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <h2>How Popular is Your Favorite Movie?</h2>
-		<Dashboard 
-			usersByMovie={this.usersPerMovie} 
-			users ={users}
-			movies={movies}/>
+        <div className="container">
+          <input 
+			className="user-Input"
+			type="text" 
+			value = {userInput}
+			placeholder="Say Something" 
+			onChange = {event => this.updateInput(event.target.value)}
+		   />
+          <p className="echo">Echo:</p>
+          <p>{userInput}</p>
+        </div>
       </div>
     );
   }
